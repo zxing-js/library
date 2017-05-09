880930ebcf7951d6b072f671b1d9955c5d599f49
Adi <adrian.tosca@gmail.com>
2017-05-09 12:45:15 +0300

    removed build folder from repository

--- build/lib/BarcodeFormat.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/*
- * Direct port to TypeScript of ZXing by Adrian Toșcă
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=BarcodeFormat.js.map
