ad9413bab81b542c28a392ceb2cc57513e4ed98a
Luiz Filipe Machado Barni <machado@odahcam.com>
2017-12-22 02:05:15 -0200

    rm useless config

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -2,15 +2,6 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: './src/index.ts',
-    output: {
-        path: resolve(__dirname, 'bundles'),
-        filename: '[name].js',
-        libraryTarget: 'umd',
-        library: pascalCase(packageName),
-        // libraryExport:  LIB_NAME,
-        // will name the AMD module of the UMD build. Otherwise an anonymous define is used.
-        umdNamedDefine: true,
-    },
     resolve: {
         extensions: ['.ts', '.js', '.tsx', '.jsx'],
     },
