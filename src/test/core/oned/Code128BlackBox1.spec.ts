46644ef27f5071790651f45de1af76f043ead506
Luiz Filipe Machado Barni <machado@odahcam.com>
2018-05-15 10:51:46 -0300

    rm commented code

--- src/test/core/oned/Code128BlackBox1.spec.ts
+++ src/test/core/oned/Code128BlackBox1.spec.ts
@@ -22,7 +22,6 @@ import 'mocha';
 import BarcodeFormat from './../../../core/BarcodeFormat';
 import MultiFormatReader from './../../../core/MultiFormatReader';
 import AbstractBlackBoxSpec from './../common/AbstractBlackBox';
-// import Code128Reader from '../../../core/oned/Code128Reader';
 
 /**
  * @author Sean Owen
@@ -31,7 +30,6 @@ class Code128BlackBox1Spec extends AbstractBlackBoxSpec {
 
     public constructor() {
         super('src/test/core/resources/blackbox/code128-1', new MultiFormatReader(), BarcodeFormat.CODE_128);
-       // super('src/test/core/resources/blackbox/code128-1', new Code128Reader(), BarcodeFormat.CODE_128);
         this.addTest(6, 6, 0.0);
         this.addTest(6, 6, 180.0);
     }
