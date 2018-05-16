022cc0ea39602f449757884c8d4160a58f148e7b
Luiz Machado <machado@odahcam.com>
2018-05-16 14:52:17 -0300

    trailing whitespace

--- src/browser/BrowserQRCodeSvgWriter.ts
+++ src/browser/BrowserQRCodeSvgWriter.ts
@@ -54,18 +54,18 @@ class BrowserQRCodeSvgWriter {
         let quietZone = BrowserQRCodeSvgWriter.QUIET_ZONE_SIZE;
 
         if (hints !== null) {
-            
+
             if (undefined !== hints.get(EncodeHintType.ERROR_CORRECTION)) {
                 errorCorrectionLevel = ErrorCorrectionLevel.fromString(hints.get(EncodeHintType.ERROR_CORRECTION).toString());
             }
-            
+
             if (undefined !== hints.get(EncodeHintType.MARGIN)) {
                 quietZone = Number.parseInt(hints.get(EncodeHintType.MARGIN).toString(), 10);
             }
         }
 
         const code = Encoder.encode(contents, errorCorrectionLevel, hints);
-            
+
         return this.renderResult(code, width, height, quietZone);
     }
 
@@ -74,13 +74,13 @@ class BrowserQRCodeSvgWriter {
      * The output matrix uses 0 == black, 255 == white (i.e. an 8 bit greyscale bitmap).
      */
     private renderResult(code: QRCode, width: number /*int*/, height: number /*int*/, quietZone: number /*int*/): SVGSVGElement {
-        
+
         const input = code.getMatrix();
-        
+
         if (input === null) {
             throw new Exception(Exception.IllegalStateException);
         }
-        
+
         const inputWidth = input.getWidth();
         const inputHeight = input.getHeight();
         const qrWidth = inputWidth + (quietZone * 2);
@@ -89,7 +89,7 @@ class BrowserQRCodeSvgWriter {
         const outputHeight = Math.max(height, qrHeight);
 
         const multiple = Math.min(Math.floor(outputWidth / qrWidth), Math.floor(outputHeight / qrHeight));
-        
+
         // Padding includes both the quiet zone and the extra white pixels to accommodate the requested
         // dimensions. For example, if input is 25x25 the QR will be 33x33 including the quiet zone.
         // If the requested size is 200x160, the multiple will be 4, for a QR of 132x132. These will
@@ -98,9 +98,9 @@ class BrowserQRCodeSvgWriter {
         const topPadding = Math.floor((outputHeight - (inputHeight * multiple)) / 2);
 
         const svgElement = this.createSVGElement(outputWidth, outputHeight);
-        
+
         this.containerElement.appendChild(svgElement);
-        
+
         for (let inputY = 0, outputY = topPadding; inputY < inputHeight; inputY++ , outputY += multiple) {
             // Write the contents of this row of the barcode
             for (let inputX = 0, outputX = leftPadding; inputX < inputWidth; inputX++ , outputX += multiple) {
@@ -115,25 +115,25 @@ class BrowserQRCodeSvgWriter {
     }
 
     private createSVGElement(w: number, h: number): SVGSVGElement {
-        
+
         const svgElement = document.createElementNS(BrowserQRCodeSvgWriter.SVG_NS, 'svg');
-        
+
         svgElement.setAttributeNS(null, 'height', w.toString());
         svgElement.setAttributeNS(null, 'width', h.toString());
-        
+
         return svgElement;
     }
 
     private createSvgRectElement(x: number, y: number, w: number, h: number): SVGRectElement {
-        
+
         const rect = document.createElementNS(BrowserQRCodeSvgWriter.SVG_NS, 'rect');
-        
+
         rect.setAttributeNS(null, 'x', x.toString());
         rect.setAttributeNS(null, 'y', y.toString());
         rect.setAttributeNS(null, 'height', w.toString());
         rect.setAttributeNS(null, 'width', h.toString());
         rect.setAttributeNS(null, 'fill', '#000000');
-        
+
         return rect;
     }
 }
