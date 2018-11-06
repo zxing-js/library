3966e89924cc5ae541be5e0062bba43db0987e10
Philip <philipvanheemstra@hotmail.com>
2018-11-06 05:21:11 +0100

    Fixed a bug
    Changed the width and height used at the start of rotate, otherwise the second time `rotate()` is used the dimensions are wrong (the ones from the previous rotation).

--- src/browser/HTMLCanvasElementLuminanceSource.ts
+++ src/browser/HTMLCanvasElementLuminanceSource.ts
@@ -119,8 +119,8 @@ export class HTMLCanvasElementLuminanceSource extends LuminanceSource {
         const angleRadians = angle * HTMLCanvasElementLuminanceSource.DEGREE_TO_RADIANS;
 
         // Calculate and set new dimensions for temp canvas
-        const width = tempCanvasElement.width;
-        const height = tempCanvasElement.height;
+        const width = this.canvas.width;
+        const height = this.canvas.height;
         const newWidth = Math.ceil( Math.abs(Math.cos(angleRadians)) * width + Math.abs(Math.sin(angleRadians)) * height );
         const newHeight = Math.ceil( Math.abs(Math.sin(angleRadians)) * width + Math.abs(Math.cos(angleRadians)) * height );
         tempCanvasElement.width = newWidth;
