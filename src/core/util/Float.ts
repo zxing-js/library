18a7d4ba78ab800ab11579e89527578f8480af30
Luiz Filipe Machado Barni <machado@odahcam.com>
2018-12-20 18:19:48 -0200

    max integer and test asserts

--- src/core/util/Float.ts
+++ src/core/util/Float.ts
@@ -6,7 +6,7 @@ export default class Float {
   /**
    * The float max value in JS is the number max value.
    */
-  static MAX_VALUE: number = Infinity;
+  static MAX_VALUE: number = Number.MAX_SAFE_INTEGER;
 
   /**
    * SincTS has no difference between int and float, there's all numbers,
