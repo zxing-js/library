99f17a9d1809a491ab20b796d71a6526de1392d2
Luiz Filipe Machado Barni <machado@odahcam.com>
2019-01-03 10:44:50 -0200

    browser multiformat reader

---
+++ src/browser/BrowserMultiFormatReader.ts
@@ -0,0 +1,27 @@
+import { BrowserCodeReader } from './BrowserCodeReader';
+import MultiFormatReader from '../core/MultiFormatReader';
+import BinaryBitmap from '../core/BinaryBitmap';
+import Result from '../core/Result';
+import DecodeHintType from '../core/DecodeHintType';
+
+export class BrowserMultiFormatReader extends BrowserCodeReader {
+
+  protected readonly reader: MultiFormatReader;
+
+  public constructor(
+    hints: Map<DecodeHintType, any> = null,
+    timeBetweenScansMillis: number = 500
+  ) {
+    const reader = new MultiFormatReader();
+    reader.setHints(hints);
+    super(reader, timeBetweenScansMillis);
+  }
+
+  /**
+   * Overwrite decodeBitmap to call decodeWithState, which will pay
+   * attention to the hints set in the constructor function
+   */
+  protected decodeBitmap(binaryBitmap: BinaryBitmap): Result {
+    return this.reader.decodeWithState(binaryBitmap);
+  }
+}
