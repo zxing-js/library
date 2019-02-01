ae19c7fe71037b677a4e4999ebf9f24245487d2b
Luiz Filipe Machado Barni <machado@odahcam.com>
2019-02-01 14:33:03 -0200

    new utils

--- src/test/core/util/Random.ts
+++ src/test/core/util/Random.ts
@@ -9,4 +9,8 @@ export default class Random {
     public next(max: number): number {
         return Math.floor(this.r() * max);
     }
+
+    public nextInt(max: int): int {
+        return this.next(max);
+    }
 }
