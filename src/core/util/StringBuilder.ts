6245676c45e8cc137ce2794b1c4b574768f7ed89
Luiz Filipe Machado Barni <machado@odahcam.com>
2019-02-14 12:38:28 -0200

    substring method

--- src/core/util/StringBuilder.ts
+++ src/core/util/StringBuilder.ts
@@ -1,37 +1,43 @@
 export default class StringBuilder {
-    public constructor(private value: string = '') {
 
-    }
-    public append(s: string | number): StringBuilder {
-        if (typeof s === 'string') {
-            this.value += s.toString();
-        } else {
-            this.value += String.fromCharCode(s);
-        }
-        return this;
-    }
+  public constructor(private value: string = '') {
 
-    public length(): number {
-        return this.value.length;
-    }
+  }
 
-    public charAt(n: number): string {
-        return this.value.charAt(n);
+  public append(s: string | number): StringBuilder {
+    if (typeof s === 'string') {
+      this.value += s.toString();
+    } else {
+      this.value += String.fromCharCode(s);
     }
+    return this;
+  }
 
-    public deleteCharAt(n: number) {
-        this.value = this.value.substr(0, n) + this.value.substring(n + 1);
-    }
+  public length(): number {
+    return this.value.length;
+  }
 
-    public setCharAt(n: number, c: string) {
-        this.value = this.value.substr(0, n) + c + this.value.substr(n + 1);
-    }
+  public charAt(n: number): string {
+    return this.value.charAt(n);
+  }
 
-    public toString(): string {
-        return this.value;
-    }
+  public deleteCharAt(n: number) {
+    this.value = this.value.substr(0, n) + this.value.substring(n + 1);
+  }
 
-    public insert(n: number, c: string) {
-        this.value = this.value.substr(0, n) + c + this.value.substr(n + c.length);
-    }
+  public setCharAt(n: number, c: string) {
+    this.value = this.value.substr(0, n) + c + this.value.substr(n + 1);
+  }
+
+  public substring(start: int, end: int): string {
+    return this.value.substring(start, end);
+  }
+
+  public toString(): string {
+    return this.value;
+  }
+
+  public insert(n: number, c: string) {
+    this.value = this.value.substr(0, n) + c + this.value.substr(n + c.length);
+  }
 }
