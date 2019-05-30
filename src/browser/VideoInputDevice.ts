598f4085cfcaa799198892ecb08daa4ec65516da
Luiz Filipe Machado Barni <machado@odahcam.com>
2019-05-30 16:17:50 -0300

    implements MediaDeviceInfo

--- src/browser/VideoInputDevice.ts
+++ src/browser/VideoInputDevice.ts
@@ -3,13 +3,21 @@
  *
  * Video input device metadata containing the id and label of the device if available.
  */
-export class VideoInputDevice {
+export class VideoInputDevice implements MediaDeviceInfo {
 
-    /**
-     * Creates an instance of VideoInputDevice.
-     *
-     * @param {string} deviceId the video input device id
-     * @param {string} label the label of the device if available
-     */
-    public constructor(public deviceId: string, public label: string) { }
+  /** @inheritdoc */
+  readonly kind = 'videoinput';
+
+  /** @inheritdoc */
+  readonly groupId: string;
+
+  /**
+   * Creates an instance of VideoInputDevice.
+   *
+   * @param {string} deviceId the video input device id
+   * @param {string} label the label of the device if available
+   */
+  public constructor(public deviceId: string, public label: string, groupId?: string) {
+    this.groupId = groupId || undefined;
+  }
 }
