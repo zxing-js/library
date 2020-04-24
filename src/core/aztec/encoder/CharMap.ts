f5e8b814dfe6f16e2dc5762ee8ed674d4110f816
mneuber <juhu>
2020-04-24 17:16:43 +0200

    add only charcodes into Char_MAP array

--- src/core/aztec/encoder/CharMap.ts
+++ src/core/aztec/encoder/CharMap.ts
@@ -5,26 +5,30 @@ import StringUtils from '../../common/StringUtils';
 import { int } from '../../../customTypings';
 
 export function static_CHAR_MAP(CHAR_MAP: Int32Array[]): Int32Array[] {
-  CHAR_MAP[C.MODE_UPPER][' '] = 1;
+  const spaceCharCode = StringUtils.getCharCode(' ');
+  const pointCharCode = StringUtils.getCharCode('.');
+  const commaCharCode = StringUtils.getCharCode(',');
+
+  CHAR_MAP[C.MODE_UPPER][spaceCharCode] = 1;
   const zUpperCharCode = StringUtils.getCharCode('Z');
   const aUpperCharCode = StringUtils.getCharCode('A');
   for (let c: int = aUpperCharCode; c <= zUpperCharCode; c++) {
     CHAR_MAP[C.MODE_UPPER][c] = c - aUpperCharCode + 2;
   }
-  CHAR_MAP[C.MODE_LOWER][' '] = 1;
+  CHAR_MAP[C.MODE_LOWER][spaceCharCode] = 1;
   const zLowerCharCode = StringUtils.getCharCode('z');
   const aLowerCharCode = StringUtils.getCharCode('a');
   for (let c: int = aLowerCharCode; c <= zLowerCharCode; c++) {
     CHAR_MAP[C.MODE_LOWER][c] = c - aLowerCharCode + 2;
   }
-  CHAR_MAP[C.MODE_DIGIT][' '] = 1;
+  CHAR_MAP[C.MODE_DIGIT][spaceCharCode] = 1;
   const nineCharCode = StringUtils.getCharCode('9');
   const zeroCharCode = StringUtils.getCharCode('0');
   for (let c: int = zeroCharCode; c <= nineCharCode; c++) {
     CHAR_MAP[C.MODE_DIGIT][c] = c - zeroCharCode + 2;
   }
-  CHAR_MAP[C.MODE_DIGIT][','] = 12;
-  CHAR_MAP[C.MODE_DIGIT]['.'] = 13;
+  CHAR_MAP[C.MODE_DIGIT][commaCharCode] = 12;
+  CHAR_MAP[C.MODE_DIGIT][pointCharCode] = 13;
   const mixedTable = [
     '\x00',
     ' ',
@@ -56,7 +60,7 @@ export function static_CHAR_MAP(CHAR_MAP: Int32Array[]): Int32Array[] {
     '\x7f'
   ];
   for (let i: int = 0; i < mixedTable.length; i++) {
-    CHAR_MAP[C.MODE_MIXED][mixedTable[i]] = i;
+    CHAR_MAP[C.MODE_MIXED][StringUtils.getCharCode(mixedTable[i])] = i;
   }
   const punctTable = [
     '\x00',
@@ -94,7 +98,7 @@ export function static_CHAR_MAP(CHAR_MAP: Int32Array[]): Int32Array[] {
 
   for (let i: int = 0; i < punctTable.length; i++) {
     if (StringUtils.getCharCode(punctTable[i]) > 0) {
-      CHAR_MAP[C.MODE_PUNCT][punctTable[i]] = i;
+      CHAR_MAP[C.MODE_PUNCT][StringUtils.getCharCode(punctTable[i])] = i;
     }
   }
 
