5fa6c63931ae17c7e7798435c56ba839c6004fbe
Luiz Machado <luizfilipe2557@gmail.com>
2020-10-16 00:26:52 -0300

    better e2e tests

---
+++ cypress/integration/browser-datamatrix-code-reader.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/e2e/pages/qr-camera/index.html');
+  });
+
+  describe('Instantiates Data Matrix Reader', () => {
+    it('has ZXing imported into window and creates instace', () => {
+      cy.window().should('have.property', 'ZXing');
+      cy.window().then((win) => {
+        // call whatever you want on your app's window
+        // so your app methods must be exposed somehow
+        const codeReader = new win.ZXing.BrowserDatamatrixCodeReader();
+        expect(codeReader).to.not.be.null;
+      });
+    });
+  });
+
+});
