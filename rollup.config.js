2938b7ce7439f6f66bf9c978fb04389367d47ea5
Erik Hughes <erik.hughes@outlook.com>
2020-11-17 17:29:09 +0100

    feat: migrated fully to the rollup build system
    BREAKING CHANGE: new location of distributed files and removed others

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,56 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
 
-export default {
-  input: 'dist/es2015/index.js',
-  external: [
-    '@zxing/text-encoding',
-  ],
-  plugins: [
-    resolve(),
-  ],
-  context: '(globalThis || global || self || window || undefined)',
-  output: {
-    format: 'umd',
-    name: 'ZXing',
-    sourcemap: true,
-    file: 'dist/umd/index.js'
+const extensions = ['.js', '.ts'];
+
+export default [
+  {
+    input: './src/index.ts',
+    external: [
+      '@zxing/text-encoding',
+    ],
+    context: '(globalThis || global || self || window || undefined)',
+    plugins: [
+      resolve({
+        extensions,
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.lib.json',
+      }),
+    ],
+    output: [
+      {
+        name: 'ZXing',
+        file: pkg.browser,
+        format: 'umd',
+        sourcemap: true,
+        plugins: [
+          terser({
+            output: {
+              comments: function (_, comment) {
+                let text = comment.value;
+                let type = comment.type;
+                if (type === 'comment2') {
+                  // multiline comment
+                  return /@preserve|@license|@cc_on/i.test(text);
+                }
+              },
+            }
+          })
+        ],
+      },
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+      },
+    ],
   },
-};
+];
