a6ebc7eff59873189e9dce09c110b5f91f6f1807
Erik Hughes <erik.hughes@outlook.com>
2020-12-04 17:45:43 +0100

    test: only include specified files and arguments

--- .mocharc.js
+++ .mocharc.js
@@ -3,7 +3,6 @@ if (!process.env.LOG_LEVEL) process.env.LOG_LEVEL = 'info';
 log.setLevel(process.env.LOG_LEVEL)
 
 module.exports = {
-  spec: 'src/test/**/*.spec.ts',
   extension: ['js', 'ts'],
   timeout: 20000,
 }
