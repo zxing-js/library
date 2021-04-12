21ebeff658ed9cf0b0833fb7e1079ce70417a40f
Frank Weigel <frank.weigel@sap.com>
2021-04-12 07:37:36 +0200

    Add copyright banner to UMD variant

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
+import license from 'rollup-plugin-license';
+import * as path from 'path';
 
 export default {
   input: 'dist/es2015/index.js',
@@ -7,6 +9,21 @@ export default {
   ],
   plugins: [
     resolve(),
+    license({
+      sourcemap: true,
+      banner: {
+        commentStyle: 'ignored', // lets terser keep it
+        content: {
+          file: path.join(__dirname, 'LICENSE'),
+          encoding: 'utf-8',
+        },
+        data() {
+          return {
+            foo: 'foo',
+          };
+        },
+      },
+    }),
   ],
   context: '(globalThis || global || self || window || undefined)',
   output: {
