74ecd7d61b955dc3049ef79634b38425eed17fab
Luiz Filipe Machado Barni <machado@odahcam.com>
2021-04-16 14:20:11 -0300

    fix testresult issue

--- src/test/core/common/AbstractBlackBox.ts
+++ src/test/core/common/AbstractBlackBox.ts
@@ -29,10 +29,10 @@ import {
 } from '@zxing/library';
 import * as fs from 'fs';
 import * as path from 'path';
+import TestResult from '../util/TestResult';
 import SharpImageLuminanceSource from '../SharpImageLuminanceSource';
 import { assertEquals } from '../util/AssertUtils';
 import SharpImage from '../util/SharpImage';
-import TestResult from './TestResult';
 
 /*import javax.imageio.ImageIO;*/
 /*import java.awt.Graphics;*/
