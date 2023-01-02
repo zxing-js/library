07b3ab0aafe5b949d3a5d66043559be3ecb4c58c
Jeroen Verhoest <jeroen_verhoest@hotmail.com>
2023-01-02 14:31:18 +0100

    Added `assertStartsWith` and `assertEndsWith` helpers

--- src/test/core/util/AssertUtils.ts
+++ src/test/core/util/AssertUtils.ts
@@ -1,9 +1,11 @@
 import * as assert from 'assert';
 
 export default class AssertUtils {
-
-  public static typedArraysAreEqual(left: Int32Array | Uint8ClampedArray, right: Int32Array | Uint8ClampedArray, size?: number): boolean {
-
+  public static typedArraysAreEqual(
+    left: Int32Array | Uint8ClampedArray,
+    right: Int32Array | Uint8ClampedArray,
+    size?: number
+  ): boolean {
     if (!size) {
       size = Math.max(left.length, right.length);
     }
@@ -16,14 +18,26 @@ export default class AssertUtils {
 
     return true;
   }
-
 }
 
-
-export const assertEquals = <T>(actual: any, expected: T, message?: string) => assert.strictEqual(actual, expected, message);
-export const assertArrayEquals = (a: Array<any> | Uint8Array | Int32Array, b: Array<any> | Uint8Array | Int32Array) => assert.deepStrictEqual(a, b);
+export const assertEquals = <T>(actual: any, expected: T, message?: string) =>
+  assert.strictEqual(actual, expected, message);
+export const assertArrayEquals = (
+  a: Array<any> | Uint8Array | Int32Array,
+  b: Array<any> | Uint8Array | Int32Array
+) => assert.deepStrictEqual(a, b);
 export const assertFalse = x => assert.strictEqual(!!x, false);
 export const assertTrue = x => assert.strictEqual(!!x, true);
 export const assertNull = x => assert.strictEqual(x, null);
 export const assertNotNull = x => assert.notStrictEqual(x, null);
 export const assertThrow = (func, err) => assert.throws(func, err);
+export const assertStartsWith = (actual: string, expected: string) => {
+  if (!actual.startsWith(expected)) {
+    throw new Error(`expected ${actual} to start with ${expected}`);
+  }
+};
+export const assertEndsWith = (actual: string, expected: string) => {
+  if (!actual.endsWith(expected)) {
+    throw new Error(`expected ${actual} to end with ${expected}`);
+  }
+};
